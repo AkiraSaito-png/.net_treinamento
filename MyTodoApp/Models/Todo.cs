dc0ac4748102f3f93815361b9e2f1cbf58fd8c83
Akira Saito <akirassan@hotmail.com>
2022-05-09 17:43:09 -0300

    treinamento

---
+++ MyTodoApp/Models/Todo.cs
@@ -0,0 +1,26 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyTodoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+
+        [DispalyName("Título")]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public string Title { get; set; }
+
+        [DisplayName("Concluído")]
+        public bool Done { get; set; }
+
+        [DisplayName("Criado em")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        
+        [DisplayName("Última atualização")]
+        public DateTime LastUpdateDate { get; set; } = DateTime.Now;
+        
+        public string User { get; set; }
+    }
+}
